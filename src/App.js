23e46d34e553b4405bacfa1ea1464d5066fb7b99
eyeA <1573765968@qq.com>
2021-07-12 16:46:01 +0800

    first demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+function CountCan({ num }) {
+  const fontColor = num > 2 ? 'red' : 'black'
+  return (
+    <div style={{ color: fontColor }}> {num}</div>
+  )
+}
+
+function Counter() {
+  const [num, setNum] = React.useState(0)
+  // return (
+  //   <div>
+  //     <button onClick={() => setNum(num + 1)}>
+  //       <CountCan num={num} />
+  //     </button>
+  //   </div>
+  // )
+  // or
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'button',
+      {
+        onClick: function onClick() {
+          return setNum(num + 1)
+        }
+      },
+      React.createElement(
+        CountCan,
+        { num }
+      )
+    )
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter />
       </header>
     </div>
   );
